50d155a5b3ed33fe5b8204bc1ad440d19265dd13
gyb1325 <guyo@onid.oregonstate.edu>
2017-04-06 16:42:33 -0700

    upload the assignment

--- percolate/Percolation.java
+++ percolate/Percolation.java
@@ -4,113 +4,113 @@
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
- private int dim ;
- private boolean [] openindex;
- private WeightedQuickUnionUF p_grid;
- private int top;
- private int bottom;
+	private int dim;
+	private boolean [] openindex;
+	private WeightedQuickUnionUF p_grid;
+	private int top;
+	private int bottom;
 
- public Percolation(int n) throws java.lang.IllegalArgumentException {
-  if (n > 0)
-   p_grid = new WeightedQuickUnionUF(n * n + 2);
-  else
-   throw new java.lang.IllegalArgumentException();
-  openindex = new boolean[n * n];
-  dim = n;
-  bottom = n * n + 1;
-  top = n * n ;
- }
+	public Percolation(int n) throws java.lang.IllegalArgumentException {
+		if (n > 0)
+			p_grid = new WeightedQuickUnionUF(n * n + 2);
+		else
+			throw new java.lang.IllegalArgumentException();
+		openindex = new boolean[n * n];
+		dim = n;
+		bottom = n * n + 1;
+		top = n * n ;
+	}
 
- public void open(int row , int col) throws java.lang.IndexOutOfBoundsException {
-  if ((row > dim || row < 1) || (col > dim || col < 1) ) {
-   throw new java.lang.IndexOutOfBoundsException();
-  }
-  int index = dim * (row - 1) + (col - 1);
-  openindex[index] = true;
+	public void open(int row , int col) throws java.lang.IndexOutOfBoundsException {
+		if ((row > dim || row < 1) || (col > dim || col < 1) ) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		int index = dim * (row - 1) + (col - 1);
+		openindex[index] = true;
 
-  if (index >= 0 && index < dim)
-   p_grid.union(index, top);
+		if (index >= 0 && index < dim)
+			p_grid.union(index, top);
 
-  if ((index >= dim * (dim - 1) ) && (index < dim * dim ) )
-   p_grid.union(index, bottom);
+		if ((index >= dim * (dim - 1) ) && (index < dim * dim ) )
+			p_grid.union(index, bottom);
 
 
-  if ((row - 1) > 0) {
-   int index_last = dim * (row - 2) + (col - 1);
-   if (openindex[index_last])
-    p_grid.union(index, index_last);
-  }
+		if ((row - 1) > 0) {
+			int index_last = dim * (row - 2) + (col - 1);
+			if (openindex[index_last])
+				p_grid.union(index, index_last);
+		}
 
-  if ((row + 1) <= dim) {
-   int index_forward = dim * (row) + (col - 1);
-   if (openindex[index_forward] )
-    p_grid.union(index, index_forward);
-  }
-  if ((col - 1) > 0) {
-   int index_c_last = dim * (row - 1) + (col - 2);
-   if (openindex[index_c_last])
-    p_grid.union(index, index_c_last);
-  }
+		if ((row + 1) <= dim) {
+			int index_forward = dim * (row) + (col - 1);
+			if (openindex[index_forward] )
+				p_grid.union(index, index_forward);
+		}
+		if ((col - 1) > 0) {
+			int index_c_last = dim * (row - 1) + (col - 2);
+			if (openindex[index_c_last])
+				p_grid.union(index, index_c_last);
+		}
 
-  if ((col + 1) <= dim) {
-   int index_c_forward = dim * (row - 1) + (col);
-   if (openindex[index_c_forward])
-    p_grid.union(index, index_c_forward);
-  }
+		if ((col + 1) <= dim) {
+			int index_c_forward = dim * (row - 1) + (col);
+			if (openindex[index_c_forward])
+				p_grid.union(index, index_c_forward);
+		}
 
- }
+	}
 
- public boolean isOpen(int row, int col) throws java.lang.IndexOutOfBoundsException {
-  if ((row > dim || row < 1) || (col > dim || col < 1) ) {
-   throw new java.lang.IndexOutOfBoundsException();
-  }
+	public boolean isOpen(int row, int col) throws java.lang.IndexOutOfBoundsException {
+		if ((row > dim || row < 1) || (col > dim || col < 1) ) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
 
-  int index = dim * (row - 1) + (col - 1);
-  return openindex[index];
- }
+		int index = dim * (row - 1) + (col - 1);
+		return openindex[index];
+	}
 
- public boolean isFull(int row, int col) throws java.lang.IndexOutOfBoundsException {
-  if ((row > dim || row < 1) || (col > dim || col < 1) ) {
-   throw new java.lang.IndexOutOfBoundsException();
-  }
-  int index = dim * (row - 1) + (col - 1);
-  return p_grid.connected(index, top);
- }
+	public boolean isFull(int row, int col) throws java.lang.IndexOutOfBoundsException {
+		if ((row > dim || row < 1) || (col > dim || col < 1) ) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		int index = dim * (row - 1) + (col - 1);
+		return p_grid.connected(index, top);
+	}
 
- public int numberOfOpenSites() {
-  int numbers = 0;
-  for (boolean element : openindex) {
-   if (element) {
-    numbers++;
-   }
-  }
-  return numbers;
- }
+	public int numberOfOpenSites() {
+		int numbers = 0;
+		for (boolean element : openindex) {
+			if (element) {
+				numbers++;
+			}
+		}
+		return numbers;
+	}
 
- public boolean percolates() {
-  return p_grid.connected(top, bottom);
- }
+	public boolean percolates() {
+		return p_grid.connected(top, bottom);
+	}
 
 // Use for simple test
- /* public static void main(String[] args) {
-   // InputStream f = new FileInputStream("F:/percolate/input8-no.txt");
-   // scanner = new Scanner(new BufferedInputStream(f), CHARSET_NAME);
-   // scanner.useLocale(LOCALE);
-   In in = new In("F:/percolate/input8-no.txt");
-   int n = in.readInt();
-   Percolation perc = new Percolation(n);
-   while (!in.isEmpty()) {
-    int i = in.readInt();
-    int j = in.readInt();
-    perc.open(i, j);
-    if (perc.percolates()) {
-     System.out.println("It percolates");
-    }
+	/* public static void main(String[] args) {
+	  // InputStream f = new FileInputStream("F:/percolate/input8-no.txt");
+	  // scanner = new Scanner(new BufferedInputStream(f), CHARSET_NAME);
+	  // scanner.useLocale(LOCALE);
+	  In in = new In("F:/percolate/input8-no.txt");
+	  int n = in.readInt();
+	  Percolation perc = new Percolation(n);
+	  while (!in.isEmpty()) {
+	   int i = in.readInt();
+	   int j = in.readInt();
+	   perc.open(i, j);
+	   if (perc.percolates()) {
+	    System.out.println("It percolates");
+	   }
 
-   }
+	  }
 
 
-  }  */
+	 }  */
 
 
 }
---
+++ percolate/PercolationStats.java
@@ -0,0 +1,66 @@
+/**
+ * Auto Generated Java Class.
+ */
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+public class PercolationStats {
+
+	private int T;
+	private double [] sites_number;
+	private int size;
+
+	public PercolationStats(int n, int trials) {
+		if (n <= 0 || trials <= 0)
+			throw new IllegalArgumentException("n<=0 || trials <=0");
+		T = trials;
+		size = n;
+		sites_number = new double[trials];
+		for (int p = 0; p < T; p++ ) {
+			Percolation perc = new Percolation(n);
+			int open_sites = 0;
+			while (!perc.percolates()) {
+				int i = StdRandom.uniform(1, size + 1);
+				int j = StdRandom.uniform(1, size + 1);
+				if (!perc.isOpen(i, j)) {
+					perc.open(i, j);
+					open_sites++;
+				}
+			}
+			sites_number[p] = (double)open_sites / (size * size);
+		}
+	}
+
+	public double mean() {
+		return StdStats.mean(sites_number);
+
+	}
+
+	public double stddev() {
+		return StdStats.stddev(sites_number);
+	}
+
+	public double confidenceLo() {
+		return (this.mean() - Math.sqrt(this.stddev()) * 1.96 / Math.sqrt(T));
+
+	}
+
+	public double confidenceHi() {
+		return (this.mean() + Math.sqrt(this.stddev()) * 1.96 / Math.sqrt(T));
+	}
+
+	public static void main(String[] args) {
+
+		int size = Integer.parseInt(args[0]);
+		int trials = Integer.parseInt(args[1]);
+		//int size =2;
+		//int trials = 10000;
+		PercolationStats percs = new PercolationStats(size, trials);
+		System.out.println("The mean is" + percs.mean());
+		System.out.println("The stddev is" + percs.stddev());
+		System.out.println("The confidence interval is [" + percs.confidenceLo() + "," + percs.confidenceHi() + "]" );
+
+	}
+
+
+}
