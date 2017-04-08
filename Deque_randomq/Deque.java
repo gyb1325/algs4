39c53624ca9e8242e17b90d2f7696b69be021f2c
gyb1325 <guyo@onid.oregonstate.edu>
2017-04-07 21:30:57 -0700

    fix bugs, and add corner cases

--- Deque_randomq/Deque.java
+++ Deque_randomq/Deque.java
@@ -1,86 +1,136 @@
 /**
  * Auto Generated Java Class.
  */
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 import java.util.Iterator;
 
-public class Deque<Item> implements Iterable<Item> {
+public class Deque<T> implements Iterable<T> {
 	private Node first, last = null;
 	private int size_num = 0;
 
-	private class Node() {
-		Item item;
+	private class Node {
+		T item;
 		Node next;
 		Node prev;
 	}
 
 	public Deque() {
-
 	}
 
+
+
 	public boolean isEmpty() {
-		return first == null;
+		return size_num == 0;
 	}
 
 	public int size() {
 		return size_num;
 	}
 
-	public void addFirst(Item item) {
+	public void addFirst(T item) {
+		if (item == null) throw new java.lang.NullPointerException("Add a null item is not allowed");
 		Node oldfirst = first;
 		first = new Node();
 		first.item = item;
 		first.next = oldfirst;
-		if (oldfirst != null)
+		if (isEmpty()) last = first;
+		if (!isEmpty())
 			oldfirst.prev = first;
 		size_num++;
 	}
 
-	public void addLast(Item item) {
+	public void addLast(T item) {
+		if (item == null) throw new java.lang.NullPointerException("Add a null item is not allowed");
 		Node oldlast = last;
 		last = new Node();
 		last.item = item;
 		last.prev = oldlast;
 		last.next = null;
-		if (isEmpty)first = last;
+		if (isEmpty())first = last;
 		else oldlast.next = last;
 		size_num++;
 	}
 
-	public Item removeFirst() {
+	public T removeFirst() {
 		if (isEmpty())throw new java.lang.UnsupportedOperationException("The deque is empty");
-		Item item = first.item;
+		T item = first.item;
 		first = first.next;
-		first.prev = null;
+		if (first != null)first.prev = null;
+		else {
+			first = null;
+			last = null;
+		}
 		size_num--;
 		return item;
 	}
 
-	public Item removeLast() {
+	public T removeLast() {
 		if (isEmpty())throw new java.lang.UnsupportedOperationException("The deque is empty");
-		Item item = last.item;
+		T item = last.item;
 		last = last.prev;
-		last.next = null;
+		if (last != null)last.next = null;
+		else {
+			first = null;
+			last = null;
+		}
 		size_num--;
 		return item;
 	}
 
-	public Iterator<Item> iterator() {	return new Dequeiterator();	}
+	public Iterator<T> iterator() { return new Dequeiterator(); }
 
-	private class Dequeiterator implements Iterator<item> {
+	private class Dequeiterator implements Iterator<T> {
 		private Node current = first;
 
 		public boolean hasNext() {return current != null;}
-		public Item next() {
-			Item item = current.item;
+		public T next() {
+			if (current == null)throw new java.util.NoSuchElementException("The iterator has no more elements");
+			T item = current.item;
 			current = current.next;
 			return item;
+		}
+		public void remove() {
+			throw new java.lang.UnsupportedOperationException("This operation is not allowed");
 		}
 	}
 
 	public static void main(String[] args) {
+		// Deque<Integer> d = new Deque<Integer>();
+		// //d.addFirst(null);
+		// d.addFirst(1);
+		// d.addFirst(2);
+		// d.addFirst(3);
+		// StdOut.println("The size is" + d.size());
+		// d.addFirst(4);
+		// d.addLast(5);
+		// d.addLast(6);
+		// d.addLast(7);
+		// d.addLast(8);
+		// StdOut.println("The size is" + d.size());
+		// d.removeLast();
+		// d.removeLast();
+		// d.removeLast();
+		// d.removeLast();
+		// d.removeLast();
+		// d.removeLast();
+		// StdOut.println("The size is" + d.size());
+		// d.removeLast();
+		// StdOut.println("The size is" + d.size());
+		// d.removeLast();
+		// StdOut.println("The size is" + d.size());
+		// d.addFirst(4);
+		// d.addLast(5);
+		// d.addLast(6);
+		// StdOut.println("The size is" + d.size());
+		// //d.removeLast();
+		// //StdOut.println("The size is" + d.size());
+		// //d.removeFirst();
+		// //StdOut.println("The size is" + d.size());
+		// Iterator<Integer> i = d.iterator();
+		// while (i.hasNext()) {
+		// 	int a = i.next();
+		// 	StdOut.println(a);
+		//}
 
 	}
 }
