393362026c93bc3f49626449807d153615646a9e
gyb1325 <guyo@onid.oregonstate.edu>
2017-04-09 14:14:41 -0700

    finish Permutation

--- Deque_randomq/Permutation.java
+++ Deque_randomq/Permutation.java
@@ -5,13 +5,11 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
 	public static void main(String[] args) {
 		int k = Integer.parseInt(args[0]);
-		int i = k;
 		RandomizedQueue<String> sr = new RandomizedQueue<String>();
-		while ((k--) > 0) {
-			String in = StdIn.readString();
-			sr.enqueue(in);
+		while (StdIn.hasNextLine() && !StdIn.isEmpty()) {
+			sr.enqueue(StdIn.readString());
 		}
-		while ((i--) > 0) {
+		while ((k--) > 0) {
 			String out = sr.dequeue();
 			StdOut.println(out);
 		}
